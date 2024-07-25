52519ee9242485c17f818794b168f3715ed69e17
Boon Kai <boonkai_tan@mymail.sutd.edu.sg>
2024-07-25 22:41:52 +0800

    Update main.py
    test json.dumps

--- python-sql-azure/main.py
+++ python-sql-azure/main.py
@@ -5,6 +5,7 @@ from typing import Dict, Union
 from fastapi import FastAPI, HTTPException 
 import logging 
 import traceback 
+import json
  
 # Load environment variables 
 load_dotenv() 
@@ -77,12 +78,8 @@ async def get_orders():
         raise HTTPException(status_code=500, detail="Internal Server Error") 
  
 @app.get("/workcentre") 
-async def get_workcentre(): 
-    try: 
-        cursor.execute("SELECT * FROM dbo.Work_Centre$$") 
-        children = rows_to_dict(cursor, "workcentre_id")  # Replace "workcentre_id" with the actual unique column name 
-        return {"count":91,"name":"nathaniel","country":[{"country_id":"NG","probability":0.22877134457826348},{"country_id":"NE","probability":0.1335755695032996},{"country_id":"GH","probability":0.11050342568000239},{"country_id":"TT","probability":0.04007267085098988},{"country_id":"ID","probability":0.0377036420696571}]}
-    except Exception as e: 
-        logging.error(f"Error in /workcentre endpoint: {e}") 
-        logging.error(traceback.format_exc()) 
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+async def get_work_centre(): 
+    test={"name": "John",
+          "age": 30,
+          "city": "New York"}
+    return json.dumps(test)
