10c587aa4efccb8c5c8ec02796b08e9c5d901271
Boon Kai <boonkai_tan@mymail.sutd.edu.sg>
2024-07-25 14:42:32 +0800

    hehe

--- python-sql-azure/main.py
+++ python-sql-azure/main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 
 
 load_dotenv()
-connection_string  = os.getenv("AZURE_SQL_CONNECTIONSTRING")
+connection_string  = ("Driver={ODBC Driver 18 for SQL Server};Server=tcp:msodbsrv.database.windows.net,1433;Database=MSO test 1;Uid=msoadmin;Pwd=msopassword123!;")
 
 connection = pyodbc.connect(connection_string)
 cursor = connection.cursor()
@@ -22,7 +22,6 @@ print("Server Running")
 
 @app.get("/items/{id}")
 async def root(id: str):
-    cursor.execute(f"SELECT child_id FROM dbo.BOM$ WHERE parent_id='{id}'")
-    cursor.execute(f"SELECT child_id FROM dbo.BOM$ WHERE parent_id='{id}'")
+    cursor.execute(f"SELECT * FROM dbo.BOM$")
     children = cursor.fetchall()
-    return str(children[0][0])
+    return str(children[0])
