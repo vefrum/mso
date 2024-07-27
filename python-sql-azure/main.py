3d4dc5f65fdf6bfca84d72995b8918ab275f81ce
Boon Kai <boonkai_tan@mymail.sutd.edu.sg>
2024-07-27 15:59:13 +0800

    Update main.py

--- python-sql-azure/main.py
+++ python-sql-azure/main.py
@@ -1,3 +1,4 @@
+import datetime
 from dotenv import load_dotenv 
 import os 
 import pyodbc 
@@ -8,7 +9,7 @@ from io import StringIO
 from pydantic import BaseModel
 from datetime import date
 
-# Global counter for Workcentre ID
+# Global counter for Workcentre ID (this part need to change to get the highest workcentre ID then +1 to it)
 workcentre_counter = 5
 
 class WorkCentre(BaseModel):
@@ -21,8 +22,46 @@ class WorkCentre(BaseModel):
     last_updated_date: date
     workcentre_id: str = None
     
+class Order(BaseModel):
+    order_id: str
+    part_name: str
+    part_id: str
+    part_qty: int
+    order_date: date
+    due_date: date
+    order_last_updated: datetime
 
+class BOM(BaseModel):
+    BOM_id: str
+    parent_id: str
+    child_id: str
+    child_qty: int
+    child_leadtime: int
+    BOM_last_updated: datetime
 
+class Routing(BaseModel):
+    routing_id: str
+    part_name: str
+    parent_id: str
+    child_id: str
+    operations_sequence: int
+    child_qty: int
+    workcentre_id: str
+    process_description: str
+    setup_time: int
+    runtime: int
+    routings_last_update: datetime
+
+class Part(BaseModel):
+    part_id: str
+    part_name: str
+    inventory: int
+    POM: str
+    UOM: str
+    part_description: str
+    unit_cost: float
+    lead_time: int
+    part_last_updated: datetime
  
 load_dotenv() 
 connection_string = os.getenv("AZURE_SQL_CONNECTIONSTRING") 
@@ -57,17 +96,107 @@ def execute_query(query: str):
 async def get_bom(): 
     query = "SELECT * FROM dbo.BOM$" 
     return execute_query(query) 
+
+@app.post("/BOM")
+async def create_bom(bom: BOM):
+    # Insert data into the database
+    
+    insert_query = """
+    INSERT INTO dbo.dbo.BOM$(BOM_id, parent_id, child_id, child_qty, child_leadtime, BOM_last_updated)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """
+    cursor.execute(insert_query, (
+        bom.BOM_id,
+        bom.parent_id,
+        bom.child_id,
+        bom.child_qty,
+        bom.child_leadtime,
+        bom.BOM_last_updated
+    ))
+    
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    response = {
+        "message": "BOM created successfully",
+        "data": bom
+    }
+    return response
  
 @app.get("/routings") 
 async def get_routings(): 
     query = "SELECT * FROM dbo.Routings$" 
     return execute_query(query) 
+
+
+@app.post("/routings")
+async def create_routing(routing: Routing):
+    # Insert data into the database
+    insert_query = """
+    INSERT INTO dbo.Routings$(routing_id, part_name, parent_id, child_id, operations_sequence, child_qty, workcentre_id, process_description, setup_time, runtime, routings_last_update)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """
+    cursor.execute(insert_query, (
+        routing.routing_id,
+        routing.part_name,
+        routing.parent_id,
+        routing.child_id,
+        routing.operations_sequence,
+        routing.child_qty,
+        routing.workcentre_id,
+        routing.process_description,
+        routing.setup_time,
+        routing.runtime,
+        routing.routings_last_update
+    ))
+    
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    response = {
+        "message": "Routing created successfully",
+        "data": routing
+    }
+    return response
  
 @app.get("/partmasterrecords") 
 async def get_part_master_records(): 
     query = "SELECT * FROM dbo.Part_Master_Records$" 
     return execute_query(query) 
+
+@app.post("/partmasterrecords")
+async def create_part(part: Part):
+    # Insert data into the database
+    
+    insert_query = """
+    INSERT INTO dbo.Part_Master_Records$(part_id, part_name, inventory, POM, UOM, part_description, unit_cost, lead_time, part_last_updated)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """
+    cursor.execute(insert_query, (
+        part.part_id,
+        part.part_name,
+        part.inventory,
+        part.POM,
+        part.UOM,
+        part.part_description,
+        part.unit_cost,
+        part.lead_time,
+        part.part_last_updated
+    ))
+    
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    response = {
+        "message": "Part created successfully",
+        "data": part
+    }
+    return response
  
+
 @app.get("/orders") 
 async def get_orders(): 
     query = "SELECT * FROM dbo.Orders$" 
@@ -80,6 +209,36 @@ async def get_orders():
     response = StreamingResponse(output, media_type="text/csv")
     response.headers["Content-Disposition"] = "attachment; filename=export_orders.csv"
     return response
+
+
+@app.post("/orders")
+async def create_order(order: Order):
+    # Insert data into the database
+    
+    insert_query = """
+    INSERT INTO dbo.dbo.Orders$(order_id, part_name, part_id, part_qty, order_date, due_date, order_last_updated)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """
+    cursor.execute(insert_query, (
+        order.order_id,
+        order.part_name,
+        order.part_id,
+        order.part_qty,
+        order.order_date,
+        order.due_date,
+        order.order_last_updated
+    ))
+    
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    response = {
+        "message": "Order created successfully",
+        "data": order
+    }
+    return response
+
  
 @app.get("/workcentre") 
 async def get_work_centre(): 
@@ -106,7 +265,7 @@ async def create_workcentre(workcentre: WorkCentre):
     workcentre_counter += 1
     
     insert_query = """
-    INSERT INTO dbo.Workcentre$ (workcentre_id, workcentre_name, workcentre_description, capacity,capacity_unit, cost_rate_h,workcentre_last_updated)
+    INSERT INTO dbo.Workcentre$(workcentre_id, workcentre_name, workcentre_description, capacity,capacity_unit, cost_rate_h,workcentre_last_updated)
     VALUES (?, ?, ?, ?, ?, ?, ?)
     """
     cursor.execute(insert_query, (
