2ff0cffc0420b8e76ff4c1a506db7799cd253069
gran-lim <152179750+gran-lim@users.noreply.github.com>
2024-08-06 09:57:36 +0800

    Update main.py

--- python-sql-azure/main.py
+++ python-sql-azure/main.py
@@ -408,15 +408,15 @@ async def update_bom(bom: BOM):
         if has_circular_dependency(bom.child_id, bom.part_id):
             return HTTPException(status_code=400, detail="Action cannot be completed: this item can't exist as both a parent and a child.")
 
-        existing_bom_query = "SELECT status FROM dbo.BOM$ WHERE BOM_id = ?"
-        cursor.execute(existing_bom_query, (bom.BOM_id,))
-        existing_bom = cursor.fetchone()
+        # existing_bom_query = "SELECT status FROM dbo.BOM$ WHERE BOM_id = ?"
+        # cursor.execute(existing_bom_query, (bom.BOM_id,))
+        # existing_bom = cursor.fetchone()
 
-        if existing_bom is None:
-            return HTTPException(status_code=404, detail=f"BOM_id {bom.BOM_id} not found")
+        # if existing_bom is None:
+        #     return HTTPException(status_code=404, detail=f"BOM_id {bom.BOM_id} not found")
 
-        if existing_bom[0] != 'active':
-            return HTTPException(status_code=400, detail="The BOM entry is not active or already updated.")
+        # if existing_bom[0] != 'active':
+        #     return HTTPException(status_code=400, detail="The BOM entry is not active or already updated.")
         
         update_status_query = """
         UPDATE dbo.BOM$
