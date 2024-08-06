5d6d7e2d262cd47a16e0ccb8b3256ecf87ce418c
gran-lim <152179750+gran-lim@users.noreply.github.com>
2024-08-06 10:13:11 +0800

    Update main.py

--- python-sql-azure/main.py
+++ python-sql-azure/main.py
@@ -422,7 +422,7 @@ async def update_bom(bom: BOM):
             update_status_query = """
             UPDATE dbo.BOM$
             SET status = 'NA'
-            WHERE BOM_id = ? AND status = 'active'
+            WHERE BOM_id = ?
             """
             cursor.execute(update_status_query, (bom.BOM_id,))
         
